12d104c96a1ecdc1ff5c588ad1255b486c2c787e
MotyaAbormotya <76623037+MotyaAbormotya@users.noreply.github.com>
2021-04-03 19:18:26 +0300

    Add files via upload

---
+++ 123/123/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace _123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirName = new DirectoryInfo(@"Загрузки");
+
+            if (dirName.Exists)
+            {
+                GetDirectoryInfo(dirName);
+            }
+            Console.ReadKey();
+        }
+
+        public static void GetDirectoryInfo(DirectoryInfo dirName)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Подкаталоги:");
+            DirectoryInfo[] subName = dirName.GetDirectories();
+            foreach (var item in subName)
+            {
+                Console.WriteLine(item.FullName);
+                GetDirectoryInfo(item);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Файлы:");
+            FileInfo[] files = dirName.GetFiles();
+            foreach (var item in files)
+            {
+                if (item.Extension == ".txt" || item.Extension == ".png")
+                    Console.WriteLine(item);
+            }
+                Console.WriteLine();
+        }
+    }
+}
